985c532b367075917a010ebf027f95533edb0e17
leyvsi <sylviecosta78500@gmail.com>
2026-07-03 13:34:05 +0200

    add import os import sys

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 from api.v1.views import app_views
