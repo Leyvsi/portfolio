a2e96d38298c033c74fc2239b33a834b0045daee
leyvsi <sylviecosta78500@gmail.com>
2026-07-06 17:23:42 +0200

    update

--- backend/api/models/user.py
+++ backend/api/models/user.py
@@ -1,11 +1,14 @@
 from api.models.base_model import BaseModel
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(BaseModel):
-    """
-    User model class
-    """
     username = ""
     email = ""
     password = ""
-    role = "user"  # Can be user or admin
+    role = "user"
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 backend_dir = os.path.abspath(os.path.join(base_dir, "../../"))
@@ -11,6 +12,19 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+def is_valid_password(password):
+    if not (8 <= len(password) <= 50):
+        return False
+    if not re.search(r"[A-Z]", password):
+        return False
+    if not re.search(r"[a-z]", password):
+        return False
+    if not re.search(r"[0-9]", password):
+        return False
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>_\-+=\[\]~/\\\';`~]", password):
+        return False
+    return True
 
 @app.after_request
 def after_request(response):
